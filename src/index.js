ed9176f54ab44dd89f86b0326369e6d231429750
Charles Howard <frodare@gmail.com>
2020-03-25 14:53:58 -0400

    cleanup

--- src/index.js
+++ src/index.js
@@ -1,12 +1,6 @@
 import Manifest from './components/Manifest'
 import Filter from './components/Filter'
 
-// TODO: save sorts in filter?
-// TODO: default page size (currenlty not well supported)
-// TODO: add loading to status message
-// TOOD: disable pageSizer and sorts when loading
-// TODO: check if the table is double pulling and make sure it only pulls once the inital values are set
-
 export {
   Manifest,
   Filter
